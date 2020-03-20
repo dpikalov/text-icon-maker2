7cac2b7955fbaf343f841019313c8aae8bb2636b
Denis Pikalov <pikalovd@gi-de.com>
2020-03-20 21:03:44 +0200

    initial commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+//
+$(function render() {
+    const ctx    = $('canvas').get(0).getContext('2d');
+    const W      = $('canvas').attr('width');
+    const H      = $('canvas').attr('height');
+
+    $('#color, #text1, #text2, #text3').on('input',() => setTimeout(render, 1))
+
+    // background color
+    ctx.fillStyle = $('#color').val();
+    ctx.fillRect(0, 0, W, H);
+
+    // text1
+    ctx.font = '64px arial';
+    ctx.textAlign = "end";
+    ctx.textBaseline = 'middle';
+    ctx.fillStyle = "#000000";
+    ctx.fillText($('#text1').val(), W * 0.9, H * 0.17);
+
+    // text2
+    ctx.font = '256px arial';
+    ctx.textAlign = "center";
+    ctx.textBaseline = 'middle';
+    ctx.fillStyle = "#000000";
+    ctx.fillText($('#text2').val(), W / 2, H / 2 + 20);
+
+    // text3
+    ctx.font = '64px arial';
+    ctx.textAlign = "end";
+    ctx.textBaseline = 'middle';
+    ctx.fillStyle = "#000000";
+    ctx.fillText($('#text3').val(), W * 0.9, H * 0.83);
+
+    // update link
+    document.getElementById('a').href = $('canvas').get(0).toDataURL()
+})
